e72b3cb57e5e6fbdb7d941f7e318bcbb1db9d12c
takeshi.fujii <isisaka3000@gmail.com>
2021-04-26 20:43:18 +0900

    __filename __dirname

---
+++ chapter1/cjs-filename-dirname.js
@@ -0,0 +1,10 @@
+module.exports = {__filename, __dirname }
+
+
+/**
+> require("./cjs-filename-dirname")
+{
+  __filename: '/Users/fujiitakeshi/workspace/handsOnNodeJs/chapter1/cjs-filename-dirname.js',
+  __dirname: '/Users/fujiitakeshi/workspace/handsOnNodeJs/chapter1'
+}
+ */
