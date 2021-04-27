867cdb2b7364b491e909a78698ae3e90162900e8
takeshi.fujii <isisaka3000@gmail.com>
2021-04-27 21:33:22 +0900

    同期的に実行されるmap

--- chapter2/main.js
+++ chapter2/main.js
@@ -5,3 +5,14 @@ setTimeout(
   },1000
 )
 console.log('setTimeout()を実行した')
+
+/**
+ コールバック=非同期　ではない
+ 例.map()メソッドは引数のコールバックを使って配列内の要素を変換するが、この処理は同期的に実行される
+ */
+const array1 = [0,1,2,3]
+const array2 = array1.map((element => {
+  console.log(`${element}を変換`)
+  return element*10
+}))
+console.log('配列の変換完了', array2)
