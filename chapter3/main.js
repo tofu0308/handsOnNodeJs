8ad117e6c0ac0448a72812735557cf5d04405ebe
takeshi.fujii <isisaka3000@gmail.com>
2021-05-25 22:00:59 +0900

    observerパターンとEventEmitter

---
+++ chapter3/main.js
@@ -0,0 +1,26 @@
+// httpモジュールでWebサーバを起動するコード
+const http = require('http')
+
+// サーバオブジェクト（EventEmitterのインスタンス）生成
+const server = http.createServer()
+
+// requestイベントのリスナ登録
+server.on('request', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.write('Hello world')
+  res.end()
+})
+
+// listening(リクエストの受付開始)イベントのリスナ登録
+server.on('listening', () => {/* ... */})
+
+// errorイベントのリスナ登録
+server.on('error', (err) => {/* ... */})
+
+// close(リクエスト受付終了)イベントのリスナ登録
+server.on('close', () => {/* ... */})
+
+// サーバー起動
+server.listen(8000)
+
+// 実行してhttp://localhost:8000/にアクセスすると画面確認可能
