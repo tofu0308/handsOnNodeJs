0d95f40bcee2c025b8d81145869932a2146c24e8
takeshi.fujii <isisaka3000@gmail.com>
2021-06-02 20:49:09 +0900

    EventEmitterの継承

--- chapter3/main.js
+++ chapter3/main.js
@@ -123,4 +123,54 @@ for(let i=0; i<100; i++) {
 events.EventEmitter.defaultMaxListeners = 100
  */
 
-// エラーハンドリング
+// EventEmitterの継承
+class FizzBuzzEventEmitter extends events.EventEmitter {
+  async start(until) {
+    this.emit('start')
+    let count = 1
+
+    while(true) {
+      if(count % 15 === 0) {
+        this.emit('FizzBuzz', count)
+      } else if (count % 3 === 0) {
+        this.emit('Fizz', count)
+      } else if (count % 5 === 0) {
+        this.emit('Buzz', count)
+      }
+
+      count += 1
+      if( count >= until) {
+        break;
+      }
+
+      await new Promise((resolve) => {setTimeout(resolve, 100)})
+    }
+    this.emit('end')
+  }
+}
+
+// 手前から移植
+function startListener() { console.log('start') }
+function fizzListener(count) { console.log('Fizz', count) }
+function buzzListener(count) { console.log('Buzz', count) }
+function fizzBuzzListener(count) { console.log('FizzBuzz', count) }
+function endListener() {
+  console.log('end')
+
+  // thisはEventEmitterのインスタンス
+  // すべてのイベントからリスナを削除する
+  this
+    .off('start', startListener)
+    .off('Fizz', fizzListener)
+    .off('Buzz', buzzListener)
+    .off('FizzBuzz', fizzBuzzListener)
+    .off('end', endListener)
+}
+
+new FizzBuzzEventEmitter()
+.on('start', startListener)
+.on('Fizz', fizzListener)
+.on('Buzz', buzzListener)
+.on('FizzBuzz', fizzBuzzListener)
+.on('end', endListener)
+.start(20)
