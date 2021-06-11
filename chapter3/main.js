0997bbe214df0ab8d53453bcb156a2f8727139ce
takeshi.fujii <isisaka3000@gmail.com>
2021-06-11 20:34:16 +0900

    stream.finished()

--- chapter3/main.js
+++ chapter3/main.js
@@ -463,3 +463,15 @@ try {
 } catch(err) {
   console.error('Error発生', err.message)
 }
+
+// ストリームの異常終了とstream.finished()
+stream.finished(
+  // dataイベントリスナを登録してフローイングモードにする
+  fs.createReadStream('src.txt').on('data', () => {}),
+  err => err ? console.error(err.message) : console.log('正常終了')
+)
+
+// util.promisify() によるPromise化
+await util.promisify(stream.finished)(
+  fs.createReadStream('src.txt').on('data', () => {})
+).then(() => console.log('正常終了'), err => console.error(err.message))
