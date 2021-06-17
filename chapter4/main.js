a6967ae9e6132913c1fa183016aee1117c2de544
takeshi.fujii <isisaka3000@gmail.com>
2021-06-17 23:02:15 +0900

    child_processモジュール

--- chapter4/main.js
+++ chapter4/main.js
@@ -1,3 +1,5 @@
+
+
 // メッセージのシリアライズ方法の指定
 const dataObj = {lastDayOfHeisei: new Date(2019, 3, 30, 9)}
 const stringifiedDataObj = JSON.stringify(dataObj)
@@ -30,3 +32,18 @@ v8.deserialize(serializeCircularWithDate)
 
 // Dateのインスタンスであることを確認
 _.lastDayOfHeisei instanceof Date
+
+
+// child_processモジュール
+const child = child_process.fork('./ipc/web-app')
+child.send(3000)
+
+// 構造化クローンアルゴリズムを使いたい場合
+const child = child_process.fork('./ipc/web-app', {serialization: 'advanced'})
+
+child_process.exec(
+  'echo "Hello, World',
+  
+  // 成功した場合、コマンドの標準出力を取得して表示
+  (err, stdout) => err? console.error(err) : console.log(stdout)
+)
