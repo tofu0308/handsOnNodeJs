374ec763a14274384327bfd97e80c81c90cad8c2
takeshi.fujii <isisaka3000@gmail.com>
2021-06-21 21:09:38 +0900

    loadtestの結果を貼付

--- chapter4/multi-thread/web-app.js
+++ chapter4/multi-thread/web-app.js
@@ -15,3 +15,27 @@ http.createServer((req, res) => {
 
 // loadtestを使った負荷テスト
 // npx loadtest -c 100 -t 10 http://localhost:3000/30
+
+/*
+[Mon Jun 21 2021 21:06:42 GMT+0900 (GMT+09:00)] INFO Requests: 0, requests per second: 0, mean latency: 0 ms
+[Mon Jun 21 2021 21:06:47 GMT+0900 (GMT+09:00)] INFO Requests: 558, requests per second: 112, mean latency: 808.5 ms
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO 
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO Target URL:          http://localhost:3000/30
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO Max time (s):        10
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO Concurrency level:   100
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO Agent:               none
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO 
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO Completed requests:  1162
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO Total errors:        0
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO Total time:          10.030008615 s
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO Requests per second: 116
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO Mean latency:        828.9 ms
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO 
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO Percentage of the requests served within a certain time
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO   50%      838 ms
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO   90%      1011 ms
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO   95%      1042 ms
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO   99%      1175 ms
+[Mon Jun 21 2021 21:06:52 GMT+0900 (GMT+09:00)] INFO  100%      1250 ms (longest request)
+
+*/
