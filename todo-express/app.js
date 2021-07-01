cba3a1a3bf8c580321c8a5d58d44608bae4b2ad9
takeshi.fujii <isisaka3000@gmail.com>
2021-07-01 22:15:10 +0900

    ToDoの新規登録をPOST

--- todo-express/app.js
+++ todo-express/app.js
@@ -8,6 +8,7 @@ let todos = [
 ]
 
 const app = express()
+app.use(express.json())
 
 // ToDo一覧の取得
 app.get('/api/todos', (req, res) => {
@@ -19,18 +20,70 @@ app.get('/api/todos', (req, res) => {
   res.json(todos.filter(todo => todo.completed === completed))
 })
 
+// ToDoのIDの値を管理するための変数
+let id = 2
+
+// ToDoの新規登録
+app.post('/api/todos', (req, res, next) => {
+  const {title} = req.body
+  if(typeof title !== 'string' || !title) {
+    // titileがリクエストに含まれない場合はステータスコード400(Bad Request)
+    const err =new Error('title is required')
+    err.statusCode = 400
+    return next(err)
+  }
+
+  // ToDoの作成
+  const todo = {id: id+=1, title, completed:false}
+  todos.push(todo)
+
+  // ステータスコード201（Created）で結果を返す
+  res.status(201).json(todo)
+}) 
+
+// エラーハンドリングミドルウェア
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.statusCode || 500).json({error: err.message})
+})
+
 app.listen(3000)
 
 /**
- node --experimental-repl-await
- require('isomorphic-fetch')
+動作確認用コード
 
- await fetch('http://localhost:3000/api/todos?completed=true')
- console.log(_.status, await _.json())
+// フィルタリングの確認
+node --experimental-repl-await
+require('isomorphic-fetch')
 
- await fetch('http://localhost:3000/api/todos?completed=false')
- console.log(_.status, await _.json())
+await fetch('http://localhost:3000/api/todos?completed=true')
+console.log(_.status, await _.json())
 
- */
+await fetch('http://localhost:3000/api/todos?completed=false')
+console.log(_.status, await _.json())
+*/
 
- 
+/**
+// fetch()でPOSTリクエストを送信
+node --experimental-repl-await
+require('isomorphic-fetch')
+
+await fetch('http://localhost:3000/api/todos', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({title: 'ペン入れ'})
+})
+
+console.log(_.status, await _.json())
+
+await fetch('http://localhost:3000/api/todos')
+console.log(_.status, await _.json())
+
+// エラーハンドリングが機能していることを確認
+await fetch('http://localhost:3000/api/todos', {method: 'POST'})
+*/
+
+
+
