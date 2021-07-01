e03ab3a479cbed2bfc98cffbdc9e35cf1986df50
takeshi.fujii <isisaka3000@gmail.com>
2021-07-01 21:46:28 +0900

    クエリパラメータに応じたフィルタリング

--- todo-express/app.js
+++ todo-express/app.js
@@ -3,14 +3,34 @@
 const express = require('express');
 
 let todos = [
-  {id: 1, title: 'ネーム', completed:'false'},
-  {id: 2, title: '下書き', completed:'true'},
+  {id: 1, title: 'ネーム', completed:false},
+  {id: 2, title: '下書き', completed:true},
 ]
 
 const app = express()
 
 // ToDo一覧の取得
-app.get('/api/todos', (req, res) => res.json(todos))
+app.get('/api/todos', (req, res) => {
+  if(!req.query.completed) {
+    return res.json(todos)
+  }
+  // completedクエリパラメータを指定された場合はToDoをフィルタリング
+  const completed = req.query.completed === 'true'
+  res.json(todos.filter(todo => todo.completed === completed))
+})
 
 app.listen(3000)
 
+/**
+ node --experimental-repl-await
+ require('isomorphic-fetch')
+
+ await fetch('http://localhost:3000/api/todos?completed=true')
+ console.log(_.status, await _.json())
+
+ await fetch('http://localhost:3000/api/todos?completed=false')
+ console.log(_.status, await _.json())
+
+ */
+
+ 
