e6ab89e226623ea4f453d51488727b167401434b
takeshi.fujii <isisaka3000@gmail.com>
2021-07-12 21:16:36 +0900

    itijihozonn

---
+++ chapter6/ws_server.js
@@ -0,0 +1,11 @@
+'use strict'
+const http = require('http')
+
+// ws
+const WebSocket = require('ws')
+
+const server = http.createServer((req, res) => {
+  // 通常のHTTPリクエストのハンドリング
+}).listen(3000)
+
+// Serverインスタンス生成
