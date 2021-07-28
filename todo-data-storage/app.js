8aba5dd3288b66d2570f1d5a79f480561115bed9
takeshi.fujii <isisaka3000@gmail.com>
2021-07-28 22:18:42 +0900

    typo

--- todo-data-storage/app.js
+++ todo-data-storage/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const {v4: uuidv4} = require('uuid')
 
 // 実行されたスクリプトの名前に応じてデータストレージの実装を使い分ける
-const dataStorage = require(`./${process.env.npm_lifesycle_event}`)
+const dataStorage = require(`./${process.env.npm_lifecycle_event}`)
 
 const app = express()
 
