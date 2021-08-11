ae2bcbcb44659d77ba7b4c80a02956f4affd21a4
takeshi.fujii <isisaka3000@gmail.com>
2021-08-11 22:34:55 +0900

    update(), remove()のテスト

--- todo-data-storage/test/mocha/data-storage.js
+++ todo-data-storage/test/mocha/data-storage.js
@@ -57,5 +57,68 @@ for (const dataStorageName of ['file-system', 'sqlite', 'leveldb']) {
         )
       })
     })
+
+    describe('update()', () => {
+      const todo1 = { id: 'a', title: 'ネーム', completed: false }
+      const todo2 = { id: 'b', title: '下書き', completed: false }
+
+      beforeEach(async () => {
+        await create(todo1)
+        await create(todo2)
+      })
+
+      it('指定したIDのToDoを更新し、更新後のToDoを返す', async () => {
+        // todo1のcomletedを更新
+        assert.deepEqual(
+          await update('a', {completed: true }),
+          { id: 'a', title: 'ネーム', completed: true }
+        )
+        assert.deepEqual(
+          await fetchByCompleted(true),
+          [{ id: 'a', title: 'ネーム', completed: true }]
+        )
+        assert.deepEqual(await fetchByCompleted(false), [todo2])
+
+        // todo2のtitleを更新
+        assert.deepEqual(
+          await update('b', { title: 'ペン入れ' }),
+          {id: 'b', title: 'ペン入れ', completed: false }
+        )
+        assert.deepEqual(
+          await fetchByCompleted(true),
+          [{ id: 'a', title: 'ネーム', completed: true }]
+        )
+        assert.deepEqual(
+          await fetchByCompleted(false),
+          [{ id: 'b', title: 'ペン入れ', completed: false }]
+        )
+      })
+      it('存在しないIDを指定するとnullを返す', async () => {
+        assert.isNull(await update('c', {completed: true}))
+        assert.deepEqual(await fetchByCompleted(true), [])
+        assert.sameDeepMembers(await fetchByCompleted(false), [todo1, todo2])
+      })
+    })
+
+    describe('remove()', () => {
+      const todo1 = { id: 'a', title: 'ネーム', completed: false }
+      const todo2 = { id: 'b', title: '下書き', completed: false }
+
+      beforeEach(async () => {
+        await create(todo1)
+        await create(todo2)
+      })
+
+      it('指定したIDのToDoを削除する', async () => {
+        assert.strictEqual(await remove('b'), 'b')
+        assert.deepEqual(await fetchAll(), [todo1])
+      })
+
+      it('存在しないIDを指定するとnull', async () => {
+        assert.strictEqual(await remove('c'), null)
+        assert.sameDeepMembers(await fetchAll(), [todo1, todo2])
+      })
+    })
+
   })
 }
