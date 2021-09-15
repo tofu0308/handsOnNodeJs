d4e36314723457ff4f13239bcdcc257e55c0f949
takeshi.fujii <isisaka3000@gmail.com>
2021-09-15 20:40:36 +0900

    不具合の修正

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,14 +1,15 @@
 'use strict'
 
 module.exports = function(num) {
+  if( num % 15 === 0) {
+    return 'FizzBuzz'
+  }
+
   if(num % 3 === 0) {
     return 'Fizz'
   }
   if(num % 5 === 0) {
     return 'Buzz'
-  }
-  if( num % 15 === 0) {
-    return 'FizzBuzz'
   }
   return String(num)
 }
